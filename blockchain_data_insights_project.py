41687ac69fbd70083974d0781ba6cfe5f7442e27
Jean Rodrigo Teixeira <167462922+jean-rodrigo-teixeira@users.noreply.github.com>
2025-03-05 16:49:15 -0300

    0.22.1 | Clean Up Project Files for Consistency
    - Removed redundant files to keep only the core project files.
    - Now, the project contains only five essential files, all named blockchain_data_insights_project:
    🔹 .csv – Dataset for analysis and reporting.
    🔹 .sql – SQL query to extract key blockchain transaction metrics.
    🔹 .py – Python script for data processing and visualization.
    🔹 .md – Documentation file with project details.
    🔹 .png – Mockup image for dashboard visualization.

--- blockchain_data_insights_project.py
+++ blockchain_data_insights_project.py
@@ -6,10 +6,24 @@ import os
 flipside = Flipside("bee16fe2-eada-494f-b5a6-9589cf47ad9d", "https://api-v2.flipsidecrypto.xyz")
 
 sql = """
-SELECT 
-    COUNT(tx_hash) AS transaction_volume
-FROM ETHEREUM.core.fact_transactions
-WHERE tx_succeeded = TRUE;
+SELECT
+    CAST(block_timestamp AS DATE) AS transaction_date,
+    COUNT(tx_hash) AS total_transactions,
+    SUM(value) AS total_value_transacted,
+    AVG(tx_fee) AS average_tx_fee,
+    COUNT(CASE WHEN gas_used > 0 THEN tx_hash END) AS transactions_with_gas,
+    SUM(gas_used * gas_price) AS total_gas_spent,
+    (COUNT(CASE WHEN tx_succeeded = TRUE THEN tx_hash END) / COUNT(tx_hash)) * 100 AS success_rate,
+    COUNT(CASE WHEN tx_type = 2 THEN tx_hash END) AS eip_1559_transactions,
+    COUNT(CASE WHEN tx_type = 0 THEN tx_hash END) AS legacy_transactions,
+    MAX(gas_price) AS max_gas_price,
+    MIN(gas_price) AS min_gas_price
+FROM
+    ethereum.core.fact_transactions
+GROUP BY
+    CAST(block_timestamp AS DATE)
+ORDER BY
+    transaction_date;
 """
 
 # Execute a consulta na Flipside
@@ -28,7 +42,7 @@ blockchain_data_path = os.getenv("blockchain_data_path")
 # Verificando se a variável de ambiente existe
 if blockchain_data_path:
     # Caminho completo para salvar o arquivo CSV
-    file_path = os.path.join(blockchain_data_path, "transaction_volume.csv")
+    file_path = os.path.join(blockchain_data_path, "blockchain_data_insights_project.csv")
     
     # Exportando o DataFrame para CSV
     df.to_csv(file_path, index=False)
--- smart_contract_interactions.sql
+++
@@ -1,9 +0,0 @@
-SELECT  
-    DATE_TRUNC('day', block_timestamp) AS transaction_date,  
-    COUNT(tx_hash) AS smart_contract_interactions  
-FROM ETHEREUM.core.fact_transactions  
-WHERE to_address IS NOT NULL  
-AND origin_function_signature IS NOT NULL  
-AND tx_succeeded = TRUE  
-GROUP BY transaction_date  
-ORDER BY transaction_date;
