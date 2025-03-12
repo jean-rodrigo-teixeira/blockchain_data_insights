aa4d4990de415e66af5143c3ad73050c74297ff6
Jean Rodrigo Teixeira <167462922+jean-rodrigo-teixeira@users.noreply.github.com>
2025-03-11 22:32:04 -0300

    0.22.4 | Update Query, Script, and Dataset
    - Updated blockchain_data_insights_project.sql to include new transaction types and smart contract interactions.
    - Modified blockchain_data_insights_project.py to process the updated dataset accordingly.
    - Downloaded and replaced blockchain_data_insights_project.csv with the latest data.
    - This update ensures a more comprehensive analysis of blockchain transactions, including EIP-2930 and Blob Transactions. 🚀

--- blockchain_data_insights_project.sql
+++ blockchain_data_insights_project.sql
@@ -8,30 +8,20 @@ SELECT
     COUNT(CASE WHEN tx_succeeded = TRUE THEN tx_hash END) AS successful_transactions,  
 
     -- Total value transacted (ETH) - Only successful transactions  
-    SUM(CASE WHEN tx_succeeded = TRUE THEN value END) AS total_value_transacted,  
+    SUM(CASE WHEN tx_succeeded = TRUE THEN value END) AS total_value,  
 
-    -- Average transaction fee (ETH) - Only successful transactions  
-    AVG(CASE WHEN tx_succeeded = TRUE THEN tx_fee END) AS average_tx_fee,  
-
-    -- Transactions with gas usage  
-    COUNT(CASE WHEN gas_used > 0 AND tx_succeeded = TRUE THEN tx_hash END) AS transactions_with_gas,  
-
-    -- Total gas spent (ETH) - Only successful transactions  
-    SUM(CASE WHEN tx_succeeded = TRUE THEN gas_used * gas_price END) AS total_gas_spent,  
-
-    -- Transaction success rate (%)  
-    (COUNT(CASE WHEN tx_succeeded = TRUE THEN tx_hash END) * 100.0 / COUNT(tx_hash)) AS success_rate,  
+    -- EIP-4844 transactions  
+    COUNT(CASE WHEN tx_type = 3 THEN tx_hash END) AS eip_4844_transactions,
 
     -- EIP-1559 transactions  
     COUNT(CASE WHEN tx_type = 2 THEN tx_hash END) AS eip_1559_transactions,  
 
-    -- Legacy transactions  
-    COUNT(CASE WHEN tx_type = 0 THEN tx_hash END) AS legacy_transactions,  
+    -- EIP-2930 transactions  
+    COUNT(CASE WHEN tx_type = 1 THEN tx_hash END) AS eip_2930_transactions,  
 
-    -- Maximum and minimum gas price (Only successful transactions)  
-    MAX(CASE WHEN tx_succeeded = TRUE THEN gas_price END) AS max_gas_price,  
-    MIN(CASE WHEN tx_succeeded = TRUE THEN gas_price END) AS min_gas_price  
+    -- Legacy transactions  
+    COUNT(CASE WHEN tx_type = 0 THEN tx_hash END) AS legacy_transactions
 
 FROM ethereum.core.fact_transactions  
-GROUP BY CAST(block_timestamp AS DATE)  
+GROUP BY CAST(block_timestamp AS DATE) 
 ORDER BY transaction_date;
